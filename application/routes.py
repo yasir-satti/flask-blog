da0b8913e2ecbacf4c156ffb332082cc5ec03e71
Yasir Satti <yassir_satti@hotmail.com>
2020-03-10 16:35:45 +0000

    created forms and env vars for security

--- application/routes.py
+++ application/routes.py
@@ -1,27 +1,41 @@
 # import render_template function from the flask module
-from flask import render_template
-# import the app object from the ./application/__init__.py
-from application import app
+from flask import render_template, redirect, url_for
 
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+# import the app object from the ./application/__init__.py
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
 
 # define routes for / & /home, this function will be called when these are accessed
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='Home', posts=blogData)
-
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
+    
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About')
+ return render_template('about.html', title='About')
+ 
+@app.route('/login')
+def login():
+ return render_template('login.html', title='Login')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )   
+
+        db.session.add(postData)
+        db.session.commit()        
+
+        return redirect(url_for('home'))    
+    else:
+        print(form.errors)    
+    return render_template('post.html', title='Post', form=form)
