30985e63a2c7999abd3e2dbfb49699af3fc38b47
Yasir Satti <yassir_satti@hotmail>
2020-03-13 14:18:01 +0000

    unit and integration testing done

---
+++ tests/test_back_end.py
@@ -0,0 +1,61 @@
+import unittest
+
+from flask import abort, url_for
+from flask_testing import TestCase
+
+from application import app, db
+from application.models import Users, Posts
+
+class TestBase(TestCase):
+
+    def create_app(self):
+
+        # pass in configurations for test database
+        config_name = 'testing'
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@35.242.185.100/test'        )
+        return app
+
+    def setUp(self):
+        """
+        Will be called before every test
+        """
+        # ensure there is no data in the test database when the test starts
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+        # create test admin user
+        admin = Users(first_name="admin", last_name="admin", email="admin@admin.com", password="admin2016")
+
+        # create test non-admin user
+        employee = Users(first_name="test", last_name="user", email="test@user.com", password="test2016")
+
+        # save users to database
+        db.session.add(admin)
+        db.session.add(employee)
+        db.session.commit()
+
+    def tearDown(self):
+        """
+        Will be called after every test
+        """
+
+        db.session.remove()
+        db.drop_all()
+
+class TestViews(TestBase):
+
+    def test_homepage_view(self):
+        """
+        Test that homepage is accessible without login
+        """
+        response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        """
+        Test that post is are redirected to the correct page.
+        """
+        response = self.client.get(url_for('post'))
+        self.assertEqual(response.status_code, 302)
